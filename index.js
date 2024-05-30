e2e5849ee6fd28e8efeed480d1c9fe885aecfb49
PrajwalPawar789 <prajwalrpawar2001@gmail.com>
2024-05-30 22:46:23 +0530

    updated dependancies

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const app = express();
-const PORT = 8002;
+// index.js
 
-app.use('/', require('./routes'));
+const { LinkedInProfileScraper } = require('linkedin-profile-scraper');
 
-//views engine setup
-app.set('view engine', 'ejs');
-app.set('views', './views');
+(async () => {
+  try {
+    const scraper = new LinkedInProfileScraper({
+      sessionCookieValue: 'AQEDAREzUtICiFl5AAABjDVNNGQAAAGOUnX25k0ARCrdOD6MjAc-Wcu-BgfMhA2WnJ6E_Hv2_hgfnh8gP2qjmIH5pPi9bN2oGurRHShdajvHLriZ_vivvX-OBfJ9LYPA7R1DaSRkdZrxzRy6tV_8ds4J',
+      keepAlive: true // Set to true for faster recurring scrapes
+    });
 
-app.listen(PORT, function(err){
-    if(err){
-        console.log(err);
+    // Login into LinkedIn
+    await scraper.setup();
+
+    // Run the scraper for the provided LinkedIn profile URL
+    const result = await scraper.run('https://www.linkedin.com/in/natfriedman/');
+
+    console.log(result);
+  } catch (err) {
+    if (err.name === 'SessionExpired') {
+      // Do something when the scraper notifies you it's not logged-in anymore
+      console.error('LinkedIn session expired. Please obtain a new session cookie value.');
+    } else {
+      console.error('Error:', err.message);
     }
-    
-    console.log("Running of port:",PORT);
-})
-
+  }
+})();
